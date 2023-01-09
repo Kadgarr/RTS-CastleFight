40c38d1f9130e071611c5d6cee6271f7076942aa
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2023-01-09 13:13:54 +0100

    First firing feature
    Added unit firing featiure;

---
+++ RTS/Assets/Scripts/Units/UnitProjectile.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitProjectile : NetworkBehaviour
+{
+    [SerializeField] private Rigidbody rbody=null;
+
+    [SerializeField] private float destroyAfterSeconds = 5f;
+
+    [SerializeField] private float launchForce = 10f;
+
+    private void Start()
+    {
+        rbody.velocity = transform.forward * launchForce;
+    }
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [Server]
+    private void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+}
