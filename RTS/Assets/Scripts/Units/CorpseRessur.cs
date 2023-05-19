55ecefc33077714f0255999b7208bf1f7880fccf
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2023-05-19 13:29:11 +0200

    Corpse
    Created Corpse.prefab and CorpRessur.cs

---
+++ RTS/Assets/Scripts/Units/CorpseRessur.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class CorpseRessur : NetworkBehaviour
+{
+
+    [SerializeField]
+    private GameObject unitToSpawn = null;
+
+    [Server]
+    public void Ressurection()
+    {
+        GameObject unitInstance = null;
+
+        unitInstance = Instantiate(unitToSpawn, this.gameObject.transform.position, this.gameObject.transform.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+}
